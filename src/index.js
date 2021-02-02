7c223dab399b47d7c5c1f260cc65116d036eeeac
ruslannovik <novik.rv93@gmail.com>
2021-02-02 11:10:29 +0300

    feat: add toReadable()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const unitsAndTensText = [
+        '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+        'sixteen', 'seventeen', 'eighteen', 'nineteen'
+    ];
+    const tensText = [
+        '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+        'seventy', 'eighty', 'ninety'
+    ];
+
+    const hundreds = Math.floor(number / 100);
+    const tens = Math.floor(number % 100 / 10);
+    const units = number % 10;
+    const tensBeforeTwenty = number % 100;
+    let result = '';
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number >= 100) {
+        result = unitsAndTensText[hundreds] + ' hundred ';
+    }
+
+    if (tens >= 2) {
+        result += tensText[tens];
+
+        if (units) {
+            result += ' ' + unitsAndTensText[units];
+        }
+    } else if (tens) {
+        result += unitsAndTensText[tensBeforeTwenty];
+    } else {
+        result += unitsAndTensText[units];
+    }
+
+    return result.trim();
 }
